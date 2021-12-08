57ee52e4be3a705a14b2a026e59895299d18a374
trevorwrightdev <trevor.wright.710@gmail.com>
2021-12-08 13:59:25 -0700

    Added banner img

---
+++ src/components/Banner.js
@@ -0,0 +1,11 @@
+import bannerImg from '../images/banner.gif'
+
+const Banner = () => {
+    return (
+        <div className='banner'>
+            <img src={bannerImg}/>
+        </div>
+    )
+}
+
+export default Banner
