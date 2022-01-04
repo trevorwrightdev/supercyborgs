b2d461fc6ef2ddeba80f6d45b705c2b851821b35
trevorwrightdev <trevor.wright.710@gmail.com>
2022-01-04 16:59:22 -0700

    Added build

---
+++ next/serverless/chunks/52.js
@@ -0,0 +1,48 @@
+"use strict";
+exports.id = 52;
+exports.ids = [52];
+exports.modules = {
+
+/***/ 8510:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(5893);
+
+
+function MyApp({ Component , pageProps  }) {
+    return(/*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(Component, {
+        ...pageProps
+    }));
+}
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (MyApp);
+
+
+/***/ }),
+
+/***/ 7020:
+/***/ ((module) => {
+
+module.exports = JSON.parse('{"polyfillFiles":["static/chunks/polyfills-5cd94c89d3acac5f.js"],"devFiles":[],"ampDevFiles":[],"lowPriorityFiles":["static/Qqw3SqrRgR7PR9x1THTFQ/_buildManifest.js","static/Qqw3SqrRgR7PR9x1THTFQ/_ssgManifest.js","static/Qqw3SqrRgR7PR9x1THTFQ/_middlewareManifest.js"],"pages":{"/":["static/chunks/webpack-514908bffb652963.js","static/chunks/framework-91d7f78b5b4003c8.js","static/chunks/main-deb592798b94b511.js","static/css/43578dd166b53229.css","static/chunks/pages/index-34342b054b6edc62.js"],"/_app":["static/chunks/webpack-514908bffb652963.js","static/chunks/framework-91d7f78b5b4003c8.js","static/chunks/main-deb592798b94b511.js","static/css/f0670d3ab14b5ee2.css","static/chunks/pages/_app-9cd1d19dd7237c4c.js"],"/_error":["static/chunks/webpack-514908bffb652963.js","static/chunks/framework-91d7f78b5b4003c8.js","static/chunks/main-deb592798b94b511.js","static/chunks/pages/_error-2280fa386d040b66.js"]},"ampFirstPages":[]}');
+
+/***/ }),
+
+/***/ 3978:
+/***/ ((module) => {
+
+module.exports = {};
+
+/***/ }),
+
+/***/ 9450:
+/***/ ((module) => {
+
+module.exports = {"Dg":[]};
+
+/***/ })
+
+};
+;
