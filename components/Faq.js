03566112bccc30d183c6bd92763acf2ae9e6a6a6
trevorwrightdev <trevor.wright.710@gmail.com>
2022-01-05 10:44:42 -0700

    Fixed apostrophe thing

--- components/Faq.js
+++ components/Faq.js
@@ -5,7 +5,7 @@ const Faq = () => {
         <div className={homeStyles.faq}>
             <h1 className={homeStyles.title}>FAQ</h1>
             <h2 className={homeStyles.question}>What is the mint price?</h2>
-            <h3 className={homeStyles.answer}>The mint price is free. It's one mint per whitelisted wallet.</h3>
+            <h3 className={homeStyles.answer}>The mint price is free. It&apos;s one mint per whitelisted wallet.</h3>
             <h2 className={homeStyles.question}>What is the total supply?</h2>
             <h3 className={homeStyles.answer}>The total supply is 3000.</h3>
             <h2 className={homeStyles.question}>When does the MMO release?</h2>
