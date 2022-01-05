229084a4821d979f0cb72cb43a96931a4c79c420
trevorwrightdev <trevor.wright.710@gmail.com>
2022-01-05 14:57:16 -0700

    Fixed up roadmap

--- components/Meta.js
+++ components/Meta.js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 const Meta = () => {
     return (
         <Head>
-            <meta charset="utf-8" />
+            <meta charSet="utf-8" />
             <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
             <title>Super Cyborgs</title>
             <meta name="description" content="" />
