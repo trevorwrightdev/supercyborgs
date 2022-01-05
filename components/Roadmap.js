a4c3a50c9eb260cc856ab4cc2a9e0fa6cfec192b
trevorwrightdev <trevor.wright.710@gmail.com>
2022-01-05 14:58:34 -0700

    Changed rebirth to identity

--- components/Roadmap.js
+++ components/Roadmap.js
@@ -8,7 +8,7 @@ const Roadmap = () => {
             <h1 className={homeStyles.title}>ROADMAP</h1>
             {/* use images here before each phase title */}
             <h2>STAGE 1</h2>
-            <h3>Rebirth</h3>
+            <h3>Identity</h3>
             <h2>STAGE 2</h2>
             <h3>Civilization</h3>
             <h2>STAGE 3</h2>
