26b1a46fb674a9dc1b2b661488dbfbb137f9c1b9
trevorwrightdev <trevor.wright.710@gmail.com>
2022-01-04 17:56:13 -0700

    Added build

---
+++ next/static/3sGJTQuMAhY1anMP5gd_2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/43578dd166b53229.css","static/chunks/pages/index-34342b054b6edc62.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
