addef0dc11b03f92b2dd85aebe757a5486d93eb3
trevorwrightdev <trevor.wright.710@gmail.com>
2022-01-05 18:09:41 -0700

    Changed metadata

--- components/Meta.js
+++ components/Meta.js
@@ -6,7 +6,10 @@ const Meta = () => {
             <meta charSet="utf-8" />
             <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
             <title>Super Cyborgs</title>
-            <meta name="description" content="" />
+            <meta name="description" content="
+            Super Cyborgs are citizens of NEON CITY, the most technologically advanced city
+            in the whole galaxy. It is the year 2099, and people have started to become more machine than man. 
+            " />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Head>
     )
