3fa0180402ee8b63ff2279b767b5b20cf2528d2b
trevorwrightdev <trevor.wright.710@gmail.com>
2022-01-07 13:42:14 -0700

    Finished the site basically

--- components/Team.js
+++ components/Team.js
@@ -1,6 +1,7 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import einstein from '../public/images/einsteinscythe.png'
+import WrappedImage from './WrappedImage'
+
+import einstein from '../public/images/EinsteinScythe.png'
+import greenDude from '../public/images/greendude.png'
 
 import homeStyles from '../styles/Home.module.css'
 
@@ -8,8 +9,12 @@ const Team = () => {
     return (
         <div className={homeStyles.team}>
             <h1 className={homeStyles.title}>THE TEAM</h1>
-            <Image className={homeStyles.teamImage} src={einstein} quality={100}/>
-            <Link className={homeStyles.link} href="https://twitter.com/trevdev_eth">@trevdev_eth</Link>
+            <WrappedImage className={homeStyles.teamImage} src={einstein} quality={100}/>
+            <span className={homeStyles.dev}>Developer</span>
+            <a className={homeStyles.link} href="https://twitter.com/trevdev_eth" target="_blank" rel="noopener noreferrer">@trevdev_eth</a>
+            <WrappedImage className={homeStyles.teamImage} src={greenDude} quality={100}/>
+            <span className={homeStyles.dev}>Artist</span>
+            <a className={homeStyles.link} href="https://www.youtube.com/c/YoogChannel" target="_blank" rel="noopener noreferrer">yoog</a>
         </div>
     )
 }
