b6ea0f0e3a8da49402e4909ad04100ab5c335e1a
trevorwrightdev <trevor.wright.710@gmail.com>
2022-02-02 19:49:29 -0700

    Changed mint price to sol

--- components/Faq.js
+++ components/Faq.js
@@ -5,7 +5,7 @@ const Faq = () => {
         <div className={homeStyles.faq}>
             <h1 className={homeStyles.title}>FAQ</h1>
             <h2 className={homeStyles.question}>What is the mint price?</h2>
-            <h3 className={homeStyles.answer}>The mint price is free. It&apos;s one mint per whitelisted wallet.</h3>
+            <h3 className={homeStyles.answer}>The mint price is <span id={homeStyles.sol}>0.5 SOL</span>. It&apos;s one mint per whitelisted wallet.</h3>
             <h2 className={homeStyles.question}>What is the total supply?</h2>
             <h3 className={homeStyles.answer}>The total supply is 3333.</h3>
             <h2 className={homeStyles.question}>When does the MMO release?</h2>
