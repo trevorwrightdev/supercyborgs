6b5258ed46df8153cf99e4cb2c3f527bf22cefa3
trevorwrightdev <trevor.wright.710@gmail.com>
2022-02-02 17:23:27 -0700

    Updated total supply in faq

--- components/Faq.js
+++ components/Faq.js
@@ -7,7 +7,7 @@ const Faq = () => {
             <h2 className={homeStyles.question}>What is the mint price?</h2>
             <h3 className={homeStyles.answer}>The mint price is free. It&apos;s one mint per whitelisted wallet.</h3>
             <h2 className={homeStyles.question}>What is the total supply?</h2>
-            <h3 className={homeStyles.answer}>The total supply is 3000.</h3>
+            <h3 className={homeStyles.answer}>The total supply is 3333.</h3>
             <h2 className={homeStyles.question}>When does the MMO release?</h2>
             <h3 className={homeStyles.answer}>The game will release a week or two after minting.</h3>
         </div>
