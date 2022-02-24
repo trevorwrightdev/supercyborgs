49b16852d69e33e5915e14de9182226bd58a4b3c
trevorwrightdev <trevor.wright.710@gmail.com>
2022-02-24 16:04:02 -0700

    Continued about page:

--- src/components/About.js
+++ src/components/About.js
@@ -6,12 +6,21 @@ const About = () => {
   return (
     <div className={styles.about}>
         <h1 className={styles.aboutTitle}>ABOUT ME</h1>
-        <div className={`${styles.aboutSection}`}>
+        <div className={`${styles.aboutSection} ${styles.aboutReverse}`}>
             <p>
-                It all began when a 
+                It all began when a kid found that he really loved video games.
             </p>
             <img src={einstein} alt=''/>
         </div>
+        <div className={`${styles.aboutSection} ${styles.about2}`} style={{paddingBottom: '25px'}}>
+            <p>
+                He decided that he wanted to make his own video games! But he was young and coding was hard, so he made board games instead.
+            </p>
+            <img src={einstein} alt=''/>
+        </div>
+        <p style={{textAlign: 'center', lineHeight: '20px'}}>
+            When he got older, he decided to give coding a try, so he could make real video games!
+        </p>
     </div>
   )
 }
