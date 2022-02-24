ee3ccbe62e1fa6e4bbec50a1e034d4a4b58bc709
trevorwrightdev <trevor.wright.710@gmail.com>
2022-02-24 16:28:58 -0700

    Added some about me images:

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,6 @@
 import einstein from '../assets/images/einsteinscythe-transparent.png'
+import gameHeart from '../assets/images/gameheart.png'
+import boardGame from '../assets/images/boardgame.png'
 
 import styles from '../styles/home.module.css'
 
@@ -10,13 +12,13 @@ const About = () => {
             <p>
                 It all began when a kid found that he really loved video games.
             </p>
-            <img src={einstein} alt=''/>
+            <img className={styles.gameHeart} src={gameHeart} alt=''/>
         </div>
         <div className={`${styles.aboutSection} ${styles.about2}`} style={{paddingBottom: '25px'}}>
             <p>
-                He decided that he wanted to make his own video games! But he was young and coding was hard, so he made board games instead.
+                He decided that he wanted to make his own video games! But he was young and coding was hard, so he made paper board games instead.
             </p>
-            <img src={einstein} alt=''/>
+            <img className={styles.boardGame} src={boardGame} alt=''/>
         </div>
         <p style={{textAlign: 'center', lineHeight: '20px'}}>
             When he got older, he decided to give coding a try, so he could make real video games!
