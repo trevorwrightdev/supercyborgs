88636876f52ae15f6f0f4c2daa94c84e855f02c6
trevorwrightdev <trevor.wright.710@gmail.com>
2022-02-24 15:44:27 -0700

    worked on about page a bit more

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,16 @@
+import einstein from '../assets/images/einsteinscythe-transparent.png'
+
 import styles from '../styles/home.module.css'
 
 const About = () => {
   return (
     <div className={styles.about}>
         <h1 className={styles.aboutTitle}>ABOUT ME</h1>
-        <div className={styles.about1}>
-            
+        <div className={`${styles.aboutSection}`}>
+            <p>
+                It all began when a 
+            </p>
+            <img src={einstein} alt=''/>
         </div>
     </div>
   )
