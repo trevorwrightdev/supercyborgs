8c9250894cfe79293a284ddf0dc297a7c2377340
trevorwrightdev <trevor.wright.710@gmail.com>
2022-02-24 12:02:36 -0700

    Changed to 4444

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -7,7 +7,7 @@ const Faq = () => {
             <h2 className='question'>What is the mint price?</h2>
             <h3 className='answer'>The mint price is <span id='sol'>0.5 SOL</span>.</h3>
             <h2 className='question'>What is the total supply?</h2>
-            <h3 className='answer'>The total supply is 3333.</h3>
+            <h3 className='answer'>The total supply is 4444.</h3>
             <h2 className='question'>When does the MMO release?</h2>
             <h3 className='answer'>The game will release a week or two after minting.</h3>
         </div>
