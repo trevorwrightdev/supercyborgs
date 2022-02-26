8a8cd0dc8f4d8e30752fc4c1e24bf83902609f71
trevorwrightdev <trevor.wright.710@gmail.com>
2022-02-25 18:42:50 -0700

    Increased font size when the screen gets bigger

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,7 @@ import styles from '../styles/home.module.css'
 const About = () => {
   return (
       <div className={styles.aboutContainer}>
-            <div className={styles.about}>
+        <div className={styles.about}>
             <h1 className={styles.aboutTitle}>ABOUT ME</h1>
             <div className={`${styles.aboutSection} ${styles.aboutReverse}`}>
                 <p>
@@ -24,11 +24,11 @@ const About = () => {
                 </p>
                 <img className={styles.boardGame} src={boardGame} alt=''/>
             </div>
-            <p style={{textAlign: 'center', lineHeight: '20px'}}>
+            <p className={styles.aboutP}>
                 When he got older, he decided to give coding a try, so he could make real video games!
             </p>
             <img className={styles.coding} src={coding} alt='' />
-            <p style={{textAlign: 'center', lineHeight: '20px'}}>
+            <p className={styles.aboutP}>
                 He got pretty good at coding games. He wanted to spend his life making all of the games he's dreamed of.
             </p>
             <div className={`${styles.aboutSection}`}>
@@ -43,16 +43,17 @@ const About = () => {
                 </p>
                 <img className={styles.lightbulb} src={lightbulb} alt=''/>
             </div>
-            <p style={{textAlign: 'center', lineHeight: '20px'}}>
+            <p className={styles.aboutP}>
                 What if he made his own NFT game? A 2D MMO, similar to the online browser games he played during his childhood.
                 He could make a metaverse world completely owned by its players!
             </p>
-            <p style={{textAlign: 'center', lineHeight: '20px'}}>
+            <p className={styles.aboutP}>
                 So.....
             </p>
             <img src={threeCyborgs} className={styles.threeCyborgs} alt=''/>
-            <p style={{textAlign: 'center', lineHeight: '20px'}}>
-                <span id={styles.lime}>SUPER CYBORGS WAS BORN!</span> <br /> The first game on the Solana blockchain that's actually good.
+            <h2 className={styles.cyborgsWasBorn}><span id={styles.lime}>SUPER CYBORGS WAS BORN!</span> </h2>
+            <p className={styles.aboutP}>
+                The first game on the Solana blockchain that's actually good.
             </p>
         </div>
       </div>
