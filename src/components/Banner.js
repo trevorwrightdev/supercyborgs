8ded42e53b5b011d60bb89209917f28605ef2d1e
trevorwrightdev <trevor.wright.710@gmail.com>
2022-03-29 16:44:17 -0600

    added gradient

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,6 +3,7 @@ import styles from '../styles/home.module.css'
 const Banner = ({ children }) => {
     return (
         <div className={styles.banner}>
+            <div className={styles.gradient} />
             {children}
         </div>
     )
