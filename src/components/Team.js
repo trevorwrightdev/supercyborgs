1cbf853f980808aee6a2088c6a97e8769168d970
trevorwrightdev <trevor.wright.710@gmail.com>
2022-06-07 10:57:38 -0700

    change

--- src/components/Team.js
+++ src/components/Team.js
@@ -6,13 +6,13 @@ import styles from '../styles/home.module.css'
 const Team = () => {
     return (
     <>
-        <h1 className={styles.teamTitle}>THE TEAM</h1>
+        {/* <h1 className={styles.teamTitle}>THE TEAM</h1> */}
         <div className={styles.team}>
-            <span className={styles.teamMember}>
+            {/* <span className={styles.teamMember}>
                 <img className={styles.teamImage} alt='' src={einstein} />
                 <span className={styles.job}>Developer</span>
                 <a className={styles.link} href="https://twitter.com/trevdev__" target="_blank" rel="noopener noreferrer">@trevdev__</a>
-            </span>
+            </span> */}
             <span className={styles.teamMember}>
                 <img className={styles.teamImage} alt='' src={greenDude} />
                 <span className={styles.job}>Artist</span>
