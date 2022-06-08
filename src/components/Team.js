999094dcb726492c998569bf8297d2849a14eed6
trevorwrightdev <trevor.wright.710@gmail.com>
2022-06-08 16:35:33 -0700

    added octavian as team member

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,4 +1,4 @@
-// import einstein from '../assets/images/EinsteinScythe.png'
+import einstein from '../assets/images/EinsteinScythe.png'
 import greenDude from '../assets/images/greendude.png'
 
 import styles from '../styles/home.module.css'
@@ -6,13 +6,13 @@ import styles from '../styles/home.module.css'
 const Team = () => {
     return (
     <>
-        {/* <h1 className={styles.teamTitle}>THE TEAM</h1> */}
+        <h1 className={styles.teamTitle}>THE TEAM</h1>
         <div className={styles.team}>
-            {/* <span className={styles.teamMember}>
+            <span className={styles.teamMember}>
                 <img className={styles.teamImage} alt='' src={einstein} />
                 <span className={styles.job}>Developer</span>
-                <a className={styles.link} href="https://twitter.com/trevdev__" target="_blank" rel="noopener noreferrer">@trevdev__</a>
-            </span> */}
+                <a className={styles.link} href="https://twitter.com/octavian_dev" target="_blank" rel="noopener noreferrer">@octavian_dev</a>
+            </span>
             <span className={styles.teamMember}>
                 <img className={styles.teamImage} alt='' src={greenDude} />
                 <span className={styles.job}>Artist</span>
