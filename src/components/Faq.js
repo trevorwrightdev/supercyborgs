821f89d4ac992bc337681d45621c12ff0e0bb2a7
trevorwrightdev <trevor.wright.710@gmail.com>
2022-06-08 16:47:04 -0700

    changed mint price to 1 SOL

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -5,7 +5,7 @@ const Faq = () => {
         <div className={styles.faq}>
             <h1 className={styles.title}>FAQ</h1>
             <h2 className={styles.question}>What is the mint price?</h2>
-            <h3 className={styles.answer}>The mint price is <span id={styles.sol}>0.5 SOL</span>.</h3>
+            <h3 className={styles.answer}>The mint price is <span id={styles.sol}>1 SOL</span>.</h3>
             <h2 className={styles.question}>What is the total supply?</h2>
             <h3 className={styles.answer}>The total supply is 4444.</h3>
             <h2 className={styles.question}>When does the MMO release?</h2>
